3f4cf49b1af76656acf10f01141d88310d8fb32d
Mircea-Coman <mircea.coman@physics.uu.se>
2024-02-23 18:44:53 +0100

    Removed the DataField class, started using pandas instead

--- Data.py
+++ Data.py
@@ -1,59 +1,82 @@
 import numpy as np
-from DataField import DataField
-import copy
+import pandas as pd
 
 class Data:
 
-    def __init__(self, data_dict):
-        self.data_dict = data_dict
+    def __init__(self, *args):
+        if len(args) == 0:
+            self.label_dict = {}
+            self.unit_dict = {}
+            self.concatenation_type_dict = {}
+            self.df =  pd.DataFrame()
+
+        elif len(args) == 1:
+            structure = args[0]
+            self.label_dict, self.unit_dict, self.concatenation_type_dict = self.__create_dictionaries__(structure)
+            self.df =  pd.DataFrame(columns = value_structure[:, 0])
+        elif len(args) == 2:
+            structure = args[0]
+            self.df = args[1]
+            self.label_dict, self.unit_dict, self.concatenation_type_dict = self.__create_dictionaries__(structure)
+        elif len(args) == 4:
+            self.label_dict = args[0]
+            self.unit_dict = args[1]
+            self.concatenation_type_dict =  args[2]
+            self.df = args[3]
+
+        else:
+            raise ValueError("Invalid number of arguments")
+
 
     def __getattr__(self, key):
-        if key in self.data_dict:
-            return self.data_dict[key]
+        if key in self.df.keys():
+            return self.df[key]
         else:
             raise AttributeError(f"'Data' object has no attribute '{key}'")
 
 
-    @staticmethod
-    def create_empty(index_dictionary):
-        data_dictionary = {}
-        for key in index_dictionary:
-            index = index_dictionary[key][0]
-            name = index_dictionary[key][1]
-            unit = index_dictionary[key][2]
-            concatenation_type  = index_dictionary[key][3]
-            data_dictionary[key] = DataField(np.array([]), name, unit, concatenation_type = concatenation_type)
-        return Data(data_dictionary)
+    def __create_dictionaries__(self, structure):
+        unit_dictionary = {}
+        label_dictionary = {}
+        concatenation_type_dictionary = {}
+        for i in np.arange(structure.shape[0]):
+            key = structure[i, 0]
+            label = structure[i, 1]
+            unit = structure[i, 2]
+            concatenation_type = structure[i, 3]
+            label_dictionary[key] = label
+            unit_dictionary[key] = unit
+            concatenation_type_dictionary[key] = concatenation_type
+        return label_dictionary, unit_dictionary, concatenation_type_dictionary
+
+    def get_label_of(self, key):
+        if key in self.label_dict:
+            return self.label_dict[key]
+        else:
+            raise AttributeError(f"'{key}' is not a valid data field!")
+
+    def get_unit_of(self, key):
+        if key in self.unit_dict:
+            return self.unit_dict[key]
+        else:
+            raise AttributeError(f"'{key}' is not a valid data field!")
 
 
     @staticmethod
-    def read_from_files(index_dictionary, file_paths, skiprows = 0):
-        joined_data = Data.create_empty(index_dictionary)
+    def read_from_files(structure, file_paths, header = 0, delimiter = '\t'):
+        dfs = []
         for file_path in file_paths:
-            data = Data.read_from_file(index_dictionary, file_path, skiprows = skiprows)
-            joined_data = Data.concatenate(joined_data, data)
-        return joined_data
+            dfs.append(pd.read_csv(file_path, delimiter=delimiter, names = structure[:, 0]))
+        df = pd.concat(dfs, axis=0, ignore_index=True)
+        return Data(structure, df)
 
 
     @staticmethod
     def read_from_file(index_dictionary, file_path, skiprows = 0):
-        data_dictionary = {}
-        raw_data = np.loadtxt(file_path, skiprows=skiprows)
-        for key in index_dictionary:
-            index = index_dictionary[key][0]
-            name = index_dictionary[key][1]
-            unit = index_dictionary[key][2]
-            concatenation_type  = index_dictionary[key][3]
-            data_dictionary[key] = DataField(raw_data[:, index], name, unit, concatenation_type = concatenation_type)
-        return Data(data_dictionary)
+        pd_data = pd.read_csv(file_path, delimiter=delimiter, names = structure[:, 0])
+        return Data(structure, pd_data)
 
     @staticmethod
-    def concatenate(data_1, data_2, deep_copy_first = False):
-        if deep_copy_first:
-            data_joined = copy.deepcopy(data_1)
-        else:
-            data_joined = data_1
-
-        for key in data_joined.data_dict:
-            data_joined.data_dict[key] = DataField.concatenate(data_joined.data_dict[key], data_2.data_dict[key])
-        return data_joined
+    def concatenate(data_1, data_2):
+        new_df = pd.concat([data_1.df, data_2.df], axis=0, ignore_index=True)
+        return Data(new_df, data_1.label_dict, data_1.unit_dict, data_1.concatenation_type_dict)
--- read_temp.py
+++ read_temp.py
@@ -1,30 +1,20 @@
 import numpy as np
-from DataField import DataField
 from Data import Data
 import matplotlib.pyplot as plt
+from default_file_structures import DEFAULT_TEMPERATURE_FILE
 
 file_1 = '/mnt/Mircea/Facultate/PhD/CryoDC/data/cryodc_20240215-092956.dat'
 file_2 = '/mnt/Mircea/Facultate/PhD/CryoDC/data/cryodc_20240215-104323.dat'
 
-index_dict = {
-    'temp_A':                 [0,   'Temperature CH A',     'K',    'normal'],
-    'temp_B':                 [1,   'Temperature CH B',     'K',    'normal'],
-    'temp_C':                 [2,   'Temperature CH C',     'K',    'normal'],
-    'temp_D':                 [3,   'Temperature CH D',     'K',    'normal'],
-    'temp_E':                 [4,   'Temperature CH E',     'K',    'normal'],
-    'temp_F':                 [5,   'Temperature CH F',     'K',    'normal'],
-    'timestamp':              [21,  'Timestamp',            's',    'normal']
-}
 
-data = Data.read_from_files(index_dict, [file_1, file_2])
-print(data.temp_A.name, data.temp_A.unit)
+data = Data.read_from_files(DEFAULT_TEMPERATURE_FILE, [file_1, file_2])
 
 plt.figure(figsize = (13, 8))
-plt.plot(data.timestamp.values,  data.temp_A.values)
-plt.plot(data.timestamp.values,  data.temp_B.values)
-plt.plot(data.timestamp.values,  data.temp_C.values)
-plt.plot(data.timestamp.values,  data.temp_D.values)
-plt.plot(data.timestamp.values,  data.temp_E.values)
-plt.plot(data.timestamp.values,  data.temp_F.values)
+plt.plot(data.df.timestamp,  data.df.temp_A)
+plt.plot(data.df.timestamp,  data.df.temp_B)
+plt.plot(data.df.timestamp,  data.df.temp_C)
+plt.plot(data.df.timestamp,  data.df.temp_D)
+plt.plot(data.df.timestamp,  data.df.temp_E)
+plt.plot(data.df.timestamp,  data.df.temp_F)
 
 plt.show()
